f63aa4d3908df053284668831db500c244f33a29
Pedro Ribeiro <pedrorib25@gmail.com>
2024-04-20 13:23:11 +0100

    button changes

--- discoverify-frontend/src/App.jsx
+++ discoverify-frontend/src/App.jsx
@@ -5,6 +5,16 @@ import './App.css'
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0)
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible(true);
+
+    // Automatically hide the text after 2 seconds
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 2000); // Adjust this value to control how long the text stays visible before disappearing
+  };
 
   useEffect(() => {
     fetch('http://localhost:5000/time').then(res => res.json()).then(data => {
@@ -14,9 +24,12 @@ function App() {
 
   return (
     <>
-      <button className="log-in-button">
+    <div className="App">
+      <button onClick={handleClick} className="log-in-button">
         Log in to Spotify
       </button>
+      {isVisible && <p>Successfully created </p>}
+    </div>
     </>
   )
 }
